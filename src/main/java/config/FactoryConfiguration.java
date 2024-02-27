d1c526d0cd2991b67fe50688174c00c95e8e04d2
pavithrani <pererapavithrani02@gmail.com>
2024-02-27 12:23:43 +0530

    created FactoryConfiguration class

---
+++ src/main/java/config/FactoryConfiguration.java
@@ -0,0 +1,34 @@
+package config;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class FactoryConfiguration {
+
+    private static FactoryConfiguration factoryConfiguration;
+    private final SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("hibernate.properties"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());;
+        }
+        Configuration configuration = new Configuration().addProperties(properties);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null) ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public  Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
