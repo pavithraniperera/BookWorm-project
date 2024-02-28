ea84d0d08da7a4ec7949647ea2e60d8a3e59ceec
pavithrani <pererapavithrani02@gmail.com>
2024-02-28 07:09:28 +0530

    create Login.css

---
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -0,0 +1,19 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+    public JFXButton btnUserLogin;
+    public JFXButton btnAdminLogin;
+
+
+    public void btnUserLoginOnAction(ActionEvent actionEvent) {
+
+    }
+
+
+    public void btnAdminLoginOnAction(ActionEvent actionEvent) {
+
+    }
+}
