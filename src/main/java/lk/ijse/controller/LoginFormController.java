a68240142663a0c74d8bdd5c025abc22b699f684
pavithrani <pererapavithrani02@gmail.com>
2024-02-29 07:20:36 +0530

    Add images

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -2,10 +2,17 @@ package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public JFXButton btnUserLogin;
     public JFXButton btnAdminLogin;
+    public AnchorPane root;
 
 
     public void btnUserLoginOnAction(ActionEvent actionEvent) {
@@ -14,6 +21,16 @@ public class LoginFormController {
 
 
     public void btnAdminLoginOnAction(ActionEvent actionEvent) {
+        AnchorPane anchorPane = null;
+        try {
+            anchorPane = FXMLLoader.load(getClass().getResource("/view/Admin_login_form.fxml"));
+            Scene scene = new Scene(anchorPane);
+            Stage stage = (Stage) root.getScene().getWindow();
+            stage.setScene(scene);
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
