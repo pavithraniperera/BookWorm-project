3b4ded6e6d30d61f10660d18fc3477b3b95c4bb1
pavithrani <pererapavithrani02@gmail.com>
2024-02-29 07:22:02 +0530

    Define Fxml Methods

---
+++ src/main/java/lk/ijse/controller/AdminLoginFormController.java
@@ -0,0 +1,37 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AdminLoginFormController {
+
+    @FXML
+    private JFXButton btnLogIn;
+
+    @FXML
+    private CheckBox checkBoxPw;
+
+    @FXML
+    private PasswordField hiddenTxtPassword;
+
+    @FXML
+    private TextField showTextPw;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLogInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkBoxPwOnAction(ActionEvent event) {
+
+    }
+
+}
