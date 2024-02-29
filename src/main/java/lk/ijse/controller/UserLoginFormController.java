4077e1c9ed33370502e7f6422fc6d6bc28e79480
pavithrani <pererapavithrani02@gmail.com>
2024-02-29 11:12:32 +0530

    Navigate register form

--- src/main/java/lk/ijse/controller/UserLoginFormController.java
+++ src/main/java/lk/ijse/controller/UserLoginFormController.java
@@ -3,12 +3,19 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserLoginFormController {
 
+    public AnchorPane root1;
     @FXML
     private JFXButton btnLogIn;
 
@@ -34,6 +41,16 @@ public class UserLoginFormController {
 
     @FXML
     void btnRegisterOnAction(ActionEvent event) {
+        AnchorPane anchorPane = null;
+        try {
+            anchorPane = FXMLLoader.load(getClass().getResource("/view/User_register_form.fxml"));
+            Scene scene = new Scene(anchorPane);
+            Stage stage = (Stage) root1.getScene().getWindow();
+            stage.setScene(scene);
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
