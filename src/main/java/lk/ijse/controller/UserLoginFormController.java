6e1113997301cb5f3a346f6f9dfc1ca2a5bb1940
pavithrani <pererapavithrani02@gmail.com>
2024-02-29 07:41:51 +0530

    define fxml methods

---
+++ src/main/java/lk/ijse/controller/UserLoginFormController.java
@@ -0,0 +1,45 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class UserLoginFormController {
+
+    @FXML
+    private JFXButton btnLogIn;
+
+    @FXML
+    private JFXButton btnRegister;
+
+    @FXML
+    private CheckBox checkBoxPw;
+
+    @FXML
+    private PasswordField hiddenTxtPassword;
+
+    @FXML
+    private TextField showTextPw;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLogInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkBoxPwOnAction(ActionEvent event) {
+
+    }
+
+}
