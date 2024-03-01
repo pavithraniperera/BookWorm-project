40a40e6247cc8cd63c52875179ed65f14b511892
pavithrani <pererapavithrani02@gmail.com>
2024-03-01 10:05:24 +0530

    upadte AdminDashboardFormController.java

--- src/main/java/lk/ijse/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/controller/AdminDashboardFormController.java
@@ -3,14 +3,25 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminDashboardFormController {
 
+    public AnchorPane pane2;
+    public TableView tblCheckOuts;
+    public JFXButton Dashboard;
+    public AnchorPane root;
     @FXML
     private JFXButton btnBook;
 
@@ -19,9 +30,7 @@ public class AdminDashboardFormController {
 
     @FXML
     private JFXButton btnTransaction;
-
-    @FXML
-    private JFXButton btnUser;
+    
 
     @FXML
     private TableColumn<?, ?> colBookName;
@@ -47,11 +56,20 @@ public class AdminDashboardFormController {
     @FXML
     private Label lblUser;
 
-    @FXML
-    private AnchorPane tblCheckOuts;
+
 
     @FXML
     void btnBookOnAction(ActionEvent event) {
+        Parent fxml = null;
+        try {
+            fxml = FXMLLoader.load(getClass().getResource("/view/Manage_book_form.fxml"));
+            pane2.getChildren().removeAll();
+
+            pane2.getChildren().setAll(fxml);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
@@ -65,10 +83,7 @@ public class AdminDashboardFormController {
 
     }
 
-    @FXML
-    void btnViewUserOnAction(ActionEvent event) {
-
-    }
+  
 
     @FXML
     void imgExitOnAction(MouseEvent event) {
@@ -80,4 +95,17 @@ public class AdminDashboardFormController {
 
     }
 
+    public void dashboardOnAction(ActionEvent actionEvent) {
+
+        AnchorPane anchorPane = null;
+        try {
+            anchorPane = FXMLLoader.load(getClass().getResource("/view/Admin_dashboard-form.fxml"));
+            Scene scene = new Scene(anchorPane);
+            Stage stage = (Stage) root.getScene().getWindow();
+            stage.setScene(scene);
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
