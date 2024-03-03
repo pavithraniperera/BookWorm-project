af00607e1f9c699fb48d2b639dfa886679a5d2bd
pavithrani <pererapavithrani02@gmail.com>
2024-03-03 12:02:59 +0530

    Define methods

---
+++ src/main/java/lk/ijse/controller/AddNewAdminFormController.java
@@ -0,0 +1,30 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddNewAdminFormController {
+
+    @FXML
+    private JFXButton btnAddAdmin;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPw;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnAddAdminOnAction(ActionEvent event) {
+
+    }
+
+}
