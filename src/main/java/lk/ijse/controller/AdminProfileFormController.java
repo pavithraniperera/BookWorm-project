f02822dc632b76aaab9f9bb7d6c4fee3bdc1eba1
pavithrani <pererapavithrani02@gmail.com>
2024-03-03 10:57:25 +0530

    create AdminProfileFormController

---
+++ src/main/java/lk/ijse/controller/AdminProfileFormController.java
@@ -0,0 +1,54 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AdminProfileFormController {
+
+    @FXML
+    private JFXButton btnAddAdmin;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnLogOut;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPw;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnAddAdminOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
