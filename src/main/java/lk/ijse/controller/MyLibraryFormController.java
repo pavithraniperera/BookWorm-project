dda7688012b9d4ecb58dc96492922dad7bf31029
pavithrani <pererapavithrani02@gmail.com>
2024-03-04 11:23:37 +0530

    update MyLibraryFormController

--- src/main/java/lk/ijse/controller/MyLibraryFormController.java
+++ src/main/java/lk/ijse/controller/MyLibraryFormController.java
@@ -1,5 +1,12 @@
 package lk.ijse.controller;
 
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
 public class MyLibraryFormController {
 
+    public AnchorPane pane2;
+    public javafx.scene.control.ScrollPane ScrollPane;
+    public GridPane bookGrid;
 }
