3e862130347d4c4b8152eb78cfb2e9c25a9cc4ff
pavithrani <pererapavithrani02@gmail.com>
2024-03-04 11:23:55 +0530

    Navigate navigation Bar

--- src/main/java/lk/ijse/controller/UserDashboardFormController.java
+++ src/main/java/lk/ijse/controller/UserDashboardFormController.java
@@ -3,6 +3,9 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
@@ -11,9 +14,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserDashboardFormController {
 
+    public AnchorPane pane2;
+    public AnchorPane root;
     @FXML
     private CheckBox CheckboxFilter;
 
@@ -59,21 +68,58 @@ public class UserDashboardFormController {
     @FXML
     void btnBorrowOnAction(ActionEvent event) {
 
+        Parent fxml = null;
+        try {
+            fxml = FXMLLoader.load(getClass().getResource("/view/User_view/Borrow_book_form.fxml"));
+            pane2.getChildren().removeAll();
+
+            pane2.getChildren().setAll(fxml);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnDashboardOnAction(ActionEvent event) {
 
+        AnchorPane anchorPane = null;
+        try {
+            anchorPane = FXMLLoader.load(getClass().getResource("/view/User_view/User_dashboard_form.fxml"));
+            Scene scene = new Scene(anchorPane);
+            Stage stage = (Stage) root.getScene().getWindow();
+            stage.setScene(scene);
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @FXML
     void btnHistoryOnAction(ActionEvent event) {
+        Parent fxml = null;
+        try {
+            fxml = FXMLLoader.load(getClass().getResource("/view/User_view/User_history_form.fxml"));
+            pane2.getChildren().removeAll();
+
+            pane2.getChildren().setAll(fxml);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @FXML
     void btnLibraryOnAction(ActionEvent event) {
+        Parent fxml = null;
+        try {
+            fxml = FXMLLoader.load(getClass().getResource("/view/User_view/My_library_form.fxml"));
+            pane2.getChildren().removeAll();
 
+            pane2.getChildren().setAll(fxml);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
@@ -98,6 +144,15 @@ public class UserDashboardFormController {
 
     @FXML
     void imgUserNameOnAction(MouseEvent event) {
+        Parent fxml = null;
+        try {
+            fxml = FXMLLoader.load(getClass().getResource("/view/User_view/User_profile_form.fxml"));
+            pane2.getChildren().removeAll();
+
+            pane2.getChildren().setAll(fxml);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
