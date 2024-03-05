e6172305e5999b67b1f8a415c4cd6bcd6c538900
pavithrani <pererapavithrani02@gmail.com>
2024-03-05 20:37:41 +0530

    create credential checking

--- src/main/java/lk/ijse/controller/AdminLoginFormController.java
+++ src/main/java/lk/ijse/controller/AdminLoginFormController.java
@@ -5,13 +5,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.Bo.custom.AdminBo;
+import lk.ijse.Bo.custom.Boimpl.AdminBoImpl;
+import lk.ijse.dto.AdminDto;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 
 public class AdminLoginFormController {
 
@@ -30,24 +37,60 @@ public class AdminLoginFormController {
 
     @FXML
     private TextField txtUserName;
+    private AdminBo adminBo = new AdminBoImpl();
 
     @FXML
     void btnLogInOnAction(ActionEvent event) {
-        AnchorPane anchorPane = null;
-        try {
-            anchorPane = FXMLLoader.load(getClass().getResource("/view/Admin_view/Admin_dashboard-form.fxml"));
-            Scene scene = new Scene(anchorPane);
-            Stage stage = (Stage) root.getScene().getWindow();
-            stage.setScene(scene);
-            stage.centerOnScreen();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+
+        login();
+
+
     }
 
     @FXML
     void checkBoxPwOnAction(ActionEvent event) {
 
+        if (checkBoxPw.isSelected()){
+            showTextPw.setText(hiddenTxtPassword.getText());
+            showTextPw.setVisible(true);
+            hiddenTxtPassword.setVisible(false);
+        }
+        else{
+            hiddenTxtPassword.setText(showTextPw.getText());
+            hiddenTxtPassword.setVisible(true);
+            showTextPw.setVisible(false);
+        }
+    }
+    void login(){
+        String username = txtUserName.getText();
+        String pw = hiddenTxtPassword.getText();
+        String pw1 = showTextPw.getText();
+        try {
+            List<AdminDto> adminDtoList = adminBo.getAllAdmins();
+            for (AdminDto dto : adminDtoList){
+                if (dto.getUserName().equals(username) && dto.getPassword().equals(pw) || dto.getPassword().equals(pw1)){
+                    AnchorPane anchorPane = null;
+                    try {
+                        anchorPane = FXMLLoader.load(getClass().getResource("/view/Admin_view/Admin_dashboard-form.fxml"));
+                        Scene scene = new Scene(anchorPane);
+                        Stage stage = (Stage) root.getScene().getWindow();
+                        stage.setScene(scene);
+                        stage.centerOnScreen();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    new Alert(Alert.AlertType.CONFIRMATION,"Login Successful").show();
+
+                }
+                else {
+                    new Alert(Alert.AlertType.ERROR,"your Login details are incorrect").show();
+                }
+            }
+            //System.out.println(adminDtoList);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
