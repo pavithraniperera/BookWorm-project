e08087090c07f1cfa6966838de82d5cecaab1933
pavithrani <pererapavithrani02@gmail.com>
2024-03-07 12:45:08 +0530

    created AdminBo

---
+++ src/main/java/lk/ijse/Bo/custom/AdminBo.java
@@ -0,0 +1,23 @@
+package lk.ijse.Bo.custom;
+
+import lk.ijse.dto.AdminDto;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface AdminBo {
+
+    List<AdminDto> getAllAdmins() throws SQLException;
+
+    AdminDto getAdminByEmail(String userName) throws  SQLException;
+
+    AdminDto getUserById(int id) throws SQLException;
+
+    void updateAdmin(AdminDto dto) throws SQLException;
+
+    void saveAdmin(AdminDto adminDto) throws SQLException;
+
+    void deleteAdmin(int id) throws SQLException;
+
+    AdminDto getAdminById(int adminId) throws SQLException;
+}
---
+++ src/main/java/lk/ijse/Bo/custom/Boimpl/AdminBoImpl.java
@@ -0,0 +1,62 @@
+package lk.ijse.Bo.custom.Boimpl;
+
+import lk.ijse.Bo.custom.AdminBo;
+import lk.ijse.Dao.custom.AdminDao;
+import lk.ijse.Dao.custom.daoImpl.AdminDaoImpl;
+import lk.ijse.dto.AdminDto;
+import lk.ijse.entity.Admin;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminBoImpl implements AdminBo {
+ private AdminDao adminDao = new AdminDaoImpl();
+
+    @Override
+    public List<AdminDto> getAllAdmins() throws SQLException {
+        List<Admin> entities = adminDao.loadAll();
+        List<AdminDto> dtos = new ArrayList<>();
+        for (Admin admin : entities){
+            dtos.add(new AdminDto(admin.getName(), admin.getEmail(), admin.getUserName(), admin.getPassword()));
+        }
+        return  dtos;
+
+    }
+
+    @Override
+    public AdminDto getAdminByEmail(String userName) throws SQLException {
+        Admin admin = adminDao.get(userName);
+        return new AdminDto(admin.getAdminId(), admin.getName(), admin.getEmail(),admin.getUserName(),admin.getPassword());
+    }
+
+    @Override
+    public AdminDto getUserById(int id) throws SQLException {
+        Admin admin = adminDao.getbyId(id);
+        System.out.println(id);
+        return new AdminDto(admin.getAdminId(), admin.getName(), admin.getEmail(), admin.getUserName(), admin.getPassword());
+    }
+
+    @Override
+    public void updateAdmin(AdminDto dto) throws SQLException {
+        adminDao.update(new Admin(dto.getAdminId(), dto.getName(), dto.getPassword(), dto.getUserName(), dto.getEmail()));
+
+    }
+
+    @Override
+    public void saveAdmin(AdminDto adminDto) throws SQLException {
+           adminDao.save(new Admin( adminDto.getEmail(), adminDto.getName(), adminDto.getPassword(), adminDto.getUserName() ));
+    }
+
+    @Override
+    public void deleteAdmin(int id) throws SQLException {
+        adminDao.delete(id);
+
+    }
+
+    @Override
+    public AdminDto getAdminById(int adminId) throws SQLException {
+       Admin admin = adminDao.getbyId(adminId);
+       return new AdminDto(admin.getAdminId(), admin.getName(), admin.getEmail(), admin.getUserName(), admin.getPassword());
+    }
+}
