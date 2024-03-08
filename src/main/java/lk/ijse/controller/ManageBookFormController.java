8177efe4dd0e76d4ba0b4e810e94f3f324561db3
pavithrani <pererapavithrani02@gmail.com>
2024-03-08 06:27:20 +0530

    finished ManageBookFormController

--- src/main/java/lk/ijse/controller/ManageBookFormController.java
+++ src/main/java/lk/ijse/controller/ManageBookFormController.java
@@ -1,13 +1,24 @@
 package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.Bo.custom.Boimpl.BranchBoImpl;
+import lk.ijse.Bo.custom.Boimpl.ManageBookBoImpl;
+import lk.ijse.Bo.custom.BranchBo;
+import lk.ijse.Bo.custom.ManageBookBo;
+import lk.ijse.Tm.ManageBookTm;
+import lk.ijse.dto.BookDto;
+import lk.ijse.dto.BranchDto;
+import lk.ijse.regex.RegexPattern;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class ManageBookFormController {
 
@@ -21,10 +32,10 @@ public class ManageBookFormController {
     private JFXButton btnUpdate;
 
     @FXML
-    private ComboBox<?> cmbAvalability;
+    private ComboBox<String> cmbAvalability;
 
     @FXML
-    private ComboBox<?> cmbBranch;
+    private ComboBox<String> cmbBranch;
 
     @FXML
     private TableColumn<?, ?> colAuthor;
@@ -38,11 +49,9 @@ public class ManageBookFormController {
     @FXML
     private TableColumn<?, ?> collName;
 
-    @FXML
-    private Spinner<?> spinnerCoppies;
 
     @FXML
-    private TableView<?> tblBook;
+    private TableView<ManageBookTm> tblBook;
 
     @FXML
     private TextField txtAuthor;
@@ -52,20 +61,142 @@ public class ManageBookFormController {
 
     @FXML
     private TextField txtTitle;
+    private  int id;
+    private ManageBookBo manageBookBo = new ManageBookBoImpl();
+    private BranchBo branchBo = new BranchBoImpl();
+    public void initialize(){
+        setBranches();
+        setStatus();
+        setCellvalueFactory();
+        tableListener();
+        loadAllBooks();
+
+    }
+
+
+    private void setCellvalueFactory() {
+        collName.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colAuthor.setCellValueFactory(new PropertyValueFactory<>("author"));
+        colAvail.setCellValueFactory(new PropertyValueFactory<>("status"));
+        colCopies.setCellValueFactory(new PropertyValueFactory<>("branch") );
+    }
+    private void tableListener() {
+        tblBook.getSelectionModel().selectedItemProperty().addListener((observable, oldValued, newValue) -> {
+//            System.out.println(newValue);
+            setData( newValue);
+        });
+    }
+
+    private void setData(ManageBookTm newValue) {
+        txtGenere.setText(newValue.getCategory());
+        txtAuthor.setText(newValue.getAuthor());
+        txtTitle.setText(newValue.getTitle());
+        cmbAvalability.setValue(newValue.getStatus());
+        cmbBranch.setValue(newValue.getBranch());
+        try {
+            id = manageBookBo.getBook(txtTitle.getText()).getBookId();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @FXML
     void btnAddOnAction(ActionEvent event) {
+        if (validation()){
+            try {
+                BranchDto branchDto = branchBo.getBranch(cmbBranch.getValue());
+                manageBookBo.saveBook(new BookDto(txtTitle.getText(),txtAuthor.getText(),txtGenere.getText(),  cmbAvalability.getValue(), branchDto.getBranchId()));
+                new Alert(Alert.AlertType.CONFIRMATION,"New Book Added").show();
+                loadAllBooks();
+                tblBook.refresh();
+
+            } catch (SQLException e) {
+                new Alert(Alert.AlertType.ERROR,"Failed to added new book").show();
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
 
+        try {
+            manageBookBo.deleteBook(id);
+            new Alert(Alert.AlertType.CONFIRMATION,"Book deleted").show();
+            loadAllBooks();
+            tblBook.refresh();
+        } catch (SQLException e) {
+
+            new Alert(Alert.AlertType.ERROR,"Failed to delete book").show();
+        }
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
+        if (validation()){
+            try {
+                int branchId = branchBo.getBranch(cmbBranch.getValue()).getBranchId();
+                manageBookBo.updateBook(new BookDto(id,txtTitle.getText(),txtAuthor.getText(),txtGenere.getText(),cmbAvalability.getValue(),branchId));
+                loadAllBooks();
+                tblBook.refresh();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
+        }
+    }
+    public boolean validation(){
+        if (!(Pattern.matches(String.valueOf(RegexPattern.getNamePattern()),txtTitle.getText()))){
+            new Alert(Alert.AlertType.ERROR,"Invalid BookTitle").show();
+            return  false;
+        }
+        if (!(Pattern.matches(String.valueOf(RegexPattern.getNamePattern()),txtAuthor.getText()))){
+            new Alert(Alert.AlertType.ERROR,"Invalid name").show();
+            return  false;
+        }
+        if (!(Pattern.matches(String.valueOf(RegexPattern.getNamePattern()),txtGenere.getText()))){
+            new Alert(Alert.AlertType.ERROR,"Invalid Category").show();
+            return  false;
+        }
+
+
+        return  true;
+
+    }
+    public void setBranches(){
+        ObservableList<String> branchNames = FXCollections.observableArrayList();
+        try {
+            List<BranchDto> branches = branchBo.getAllBranches();
+            for (BranchDto dto : branches){
+                branchNames.add(dto.getBranchName());
+            }
+            cmbBranch.setItems(branchNames);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void setStatus(){
+        ObservableList<String> status = FXCollections.observableArrayList("Available", "Unavailable");
+        cmbAvalability.setItems(status);
+
+
+    }
+    public void loadAllBooks(){
+        ObservableList<ManageBookTm> observableList = FXCollections.observableArrayList();
+        try {
+            List<BookDto> bookDtos = manageBookBo.getAllBook();
+            for (BookDto bookDto :bookDtos){
+                BranchDto dto = branchBo.getBranchById(bookDto.getBranchId());
+                observableList.add(new ManageBookTm(bookDto.getTitle(), bookDto.getAuthor(), bookDto.getCategory(), bookDto.getAvailability(), dto.getBranchName()));
+            }
+          tblBook.getItems().clear();
+            tblBook.setItems(observableList);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
