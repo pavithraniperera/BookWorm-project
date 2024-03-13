e1bb1d262a1bdd34c1da1b3f7c4deda46a1c6238
pavithrani <pererapavithrani02@gmail.com>
2024-03-13 20:04:42 +0530

    Create UserHistoryFormController

--- src/main/java/lk/ijse/controller/UserHistoryFormController.java
+++ src/main/java/lk/ijse/controller/UserHistoryFormController.java
@@ -1,8 +1,24 @@
 package lk.ijse.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.Bo.custom.Boimpl.TransactionBoImpl;
+import lk.ijse.Bo.custom.Boimpl.UserBoImpl;
+import lk.ijse.Bo.custom.TransactionBo;
+import lk.ijse.Bo.custom.UserBo;
+import lk.ijse.Tm.TransactionTm;
+import lk.ijse.dto.TransactionDto;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static lk.ijse.controller.UserLoginFormController.userId;
 
 public class UserHistoryFormController {
 
@@ -19,6 +35,56 @@ public class UserHistoryFormController {
     private TableColumn<?, ?> colReturnedDate;
 
     @FXML
-    private TableView<?> tblHistory;
+    private TableView<TransactionTm> tblHistory;
+    private TransactionBo transactionBo = new TransactionBoImpl();
+    private UserBo userBo = new UserBoImpl();
+    private ObservableList<TransactionTm> observableList = FXCollections.observableArrayList();
 
+
+    public void initialize(){
+      ;
+        setCellValuefactory();
+        loadAllTransactionByUser();
+
+
+    }
+
+    private void loadAllTransactionByUser() {
+        try {
+            List<TransactionDto> dtoList = transactionBo.getByUser(userId);
+            for (TransactionDto dto : dtoList){
+                if (dto.getReturn().equals(true)) {
+                    observableList.add(new TransactionTm(
+                            dto.getBookId(),
+                            formatDateTime(dto.getReturnedDate()),
+                            formatDateTime(dto.getBorrowed()),
+                            (dto.getDueDate())));
+                } else {
+                    observableList.add(new TransactionTm(
+                            dto.getBookId(),
+                            "Not returned Yet",
+                            formatDateTime(dto.getBorrowed()),
+                            (dto.getDueDate())));
+                }
+
+            }
+            tblHistory.getItems().clear();
+            tblHistory.setItems(observableList);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void setCellValuefactory() {
+        colBookTitle.setCellValueFactory(new PropertyValueFactory<>("bookName"));
+        colBorrowedDate.setCellValueFactory(new PropertyValueFactory<>("borrow"));
+        colDueDate.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+        colReturnedDate.setCellValueFactory(new PropertyValueFactory<>("returnDate"));
+    }
+    private String formatDateTime(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // Customize format (e.g., "dd/MM/yyyy")
+        return dateTime.format(formatter);
+    }
 }
