8873f07b1ba32165441ef36be5a47d0abc739150
pavithrani <pererapavithrani02@gmail.com>
2024-03-13 06:42:13 +0530

    create Book return transaction

--- src/main/java/lk/ijse/controller/BookCardFormController.java
+++ src/main/java/lk/ijse/controller/BookCardFormController.java
@@ -1,15 +1,31 @@
 package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXButton;
+import jakarta.transaction.Transactional;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import lk.ijse.Bo.custom.Boimpl.BookBoImpl;
+import lk.ijse.Bo.custom.Boimpl.TransactionBoImpl;
 import lk.ijse.Bo.custom.BookBo;
+import lk.ijse.Bo.custom.TransactionBo;
+import lk.ijse.config.FactoryConfiguration;
 import lk.ijse.dto.BookDto;
 import lk.ijse.dto.TransactionDto;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class BookCardFormController {
 
@@ -29,12 +45,91 @@ public class BookCardFormController {
     private Label lblDueDate;
     private MyLibraryFormController libraryFormController;
     private BookBo bookBo = new BookBoImpl();
+    private TransactionBo transactionBo = new TransactionBoImpl();
     private TransactionDto transactionDto ;
 
     @FXML
     void btnReturnOnAction(ActionEvent event) {
-        
 
+        try {
+            var dto= transactionBo.gettransactionByBookUser(transactionDto.getBookId(),transactionDto.getUserId());
+            System.out.println(dto.getId());
+            dto.setReturn(true);
+            dto.setReturnedDate(LocalDateTime.now());
+            BookDto dto1 =  bookBo.getBook(transactionDto.getBookId());
+            dto1.setAvailability("Available");
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Dialog");
+            alert.setHeaderText("Do you want to Return the book?");
+            alert.setContentText("Choose your option.");
+
+            ButtonType yesButton = new ButtonType("Yes");
+            ButtonType noButton = new ButtonType("No");
+
+            alert.getButtonTypes().setAll(yesButton, noButton);
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == yesButton) {
+              /*  boolean isUpdated =   transactionBo.update(dto);
+                if (isUpdated){
+                    bookBo.updateBook(dto1);
+                    new Alert(Alert.AlertType.CONFIRMATION,"return successful.").show();
+                }*/
+               updateBookAndTransaction(dto1,dto);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @Transactional
+    private void updateBookAndTransaction(BookDto dto1, TransactionDto dto) {
+      /*  Session session = FactoryConfiguration.getInstance().getSession();
+
+        if (dto!=null){
+            try {
+                Transaction transaction = session.beginTransaction();
+                boolean isUpdated =   transactionBo.update(dto,session);
+                if (isUpdated){
+                    boolean update =bookBo.updateBook(dto1,session);
+                    if (update) {
+                        transaction.commit();
+                       new Alert(Alert.AlertType.INFORMATION,"Book is Returned successfully").show();
+                    }
+                }else {
+                    transaction.rollback();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            finally {
+                session.close();
+            }
+
+        }else{
+            System.out.println("no");
+        }*/
+        if (dto != null) {
+
+            BookDto bookDto = null;
+            try {
+                transactionBo.update(dto);
+                bookDto = bookBo.getBook(dto.getBookId());
+                bookDto.setAvailability("Available");
+                bookBo.updateBook(bookDto);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            new Alert(Alert.AlertType.INFORMATION, "Book is Returned successfully").show();
+        } else {
+
+            try {
+                throw new Exception("Transaction not found.");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 
