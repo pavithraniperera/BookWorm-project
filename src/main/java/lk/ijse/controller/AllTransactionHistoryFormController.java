62236e191249935001a968556b9560c52d58e975
pavithrani <pererapavithrani02@gmail.com>
2024-03-13 20:04:14 +0530

    Create AllTransactionHistoryFormController

--- src/main/java/lk/ijse/controller/AllTransactionHistoryFormController.java
+++ src/main/java/lk/ijse/controller/AllTransactionHistoryFormController.java
@@ -1,18 +1,26 @@
 package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.Bo.custom.Boimpl.TransactionBoImpl;
+import lk.ijse.Bo.custom.TransactionBo;
+import lk.ijse.Tm.TransactionTm;
+import lk.ijse.dto.TransactionDto;
 
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class AllTransactionHistoryFormController {
 
@@ -42,13 +50,73 @@ public class AllTransactionHistoryFormController {
     private TableColumn<?, ?> colUserId;
 
     @FXML
-    private ComboBox<?> combType;
+    private ComboBox<String> combType;
 
     @FXML
-    private TableView<?> tblTransaction;
+    private TableView<TransactionTm> tblTransaction;
 
     @FXML
     private TextField txtSearch;
+    private TransactionBo transactionBo = new TransactionBoImpl();
+    private ObservableList<TransactionTm> observableList = FXCollections.observableArrayList();
+    private ObservableList<TransactionTm> userFiltered = FXCollections.observableArrayList();
+    private ObservableList<TransactionTm> typeFiltered = FXCollections.observableArrayList();
+
+    public void initialize(){
+        setCellValueFactory();
+        loadAllTransactions();
+        setComboBoxValues();
+    }
+
+    private void setComboBoxValues() {
+        ObservableList<String> list = FXCollections.observableArrayList("All","Not returned Yet");
+        combType.setItems(list);
+
+    }
+
+    private void loadAllTransactions() {
+        try {
+            List<TransactionDto> dtos = transactionBo.getAll();
+            for (TransactionDto dto : dtos){
+                if (dto.getReturn().equals(true)) {
+                    observableList.add(new TransactionTm(
+                            "T00" + dto.getId(),
+                            "B00" + dto.getBookname(),
+                            dto.getBookId(),
+                            dto.getUserName(),
+                            dto.getBranchName(),
+                            formatDateTime(dto.getBorrowed()),
+                            formatDateTime(dto.getReturnedDate())));
+                } else {
+                    observableList.add(new TransactionTm(
+                            "T00" + dto.getId(),
+                            "B00" + dto.getBookname(),
+                            dto.getBookId(),
+                            dto.getUserName(),
+                            dto.getBranchName(),
+                            formatDateTime(dto.getBorrowed()),
+                            "Not returned yet"));
+                }
+
+            }
+            tblTransaction.getItems().clear();
+            tblTransaction.setItems(observableList);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void setCellValueFactory() {
+        colTransactionId.setCellValueFactory(new PropertyValueFactory<>("transactionId"));
+        colDate.setCellValueFactory(new PropertyValueFactory<>("returnDate"));
+        colType.setCellValueFactory(new PropertyValueFactory<>("borrow"));
+        colUserId.setCellValueFactory(new PropertyValueFactory<>("userName"));
+        colBranchId.setCellValueFactory(new PropertyValueFactory<>("branchName"));
+        ColBookId.setCellValueFactory(new PropertyValueFactory<>("bookId"));
+        ColBookName.setCellValueFactory(new PropertyValueFactory<>("bookName"));
+    }
 
     @FXML
     void btnOverdueAction(ActionEvent event) {
@@ -65,12 +133,33 @@ public class AllTransactionHistoryFormController {
 
     @FXML
     void combTypeOnAction(ActionEvent event) {
-
+        typeFiltered.clear();
+        String selected = combType.getValue();
+        if (selected.equals("All")){
+            typeFiltered.addAll(observableList);
+        }else if (selected.equals("Not returned Yet")) {
+            for (TransactionTm transactionTm : observableList) {
+                if (transactionTm.getReturnDate().equals("Not returned yet")) {
+                    typeFiltered.add(transactionTm); // Add only non-returned transactions
+                }
+            }
+        }
+        tblTransaction.getItems().setAll(typeFiltered);
     }
 
     @FXML
     void txtSearchOnAction(ActionEvent event) {
-
+        userFiltered.clear();
+        for (TransactionTm transactionTm : observableList) {
+            if (transactionTm.getUserName().equals(txtSearch.getText())) {
+                userFiltered.add(transactionTm); // Add matching transactions
+            }
+        }
+        tblTransaction.getItems().setAll(userFiltered);
+    }
+    private String formatDateTime(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // Customize format (e.g., "dd/MM/yyyy")
+        return dateTime.format(formatter);
     }
 
 }
